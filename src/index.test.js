dad7dbe5ee4588850f37649220bd984df45538a1
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2017-09-14 15:26:21 +0200

    Travis CI

--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,6 @@ import {expect} from 'chai';
 
 describe("Testing Index.js", () => {
   it ("Should always pass", () => {
-    expect(true).to.equal(true);
+    expect(true).to.equal(false);
   });
 });
