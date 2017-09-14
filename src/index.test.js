7150d86875b6b363044636ebb36ea063f3593f51
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2017-09-14 15:28:52 +0200

    Fix Build

--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,6 @@ import {expect} from 'chai';
 
 describe("Testing Index.js", () => {
   it ("Should always pass", () => {
-    expect(true).to.equal(false);
+    expect(true).to.equal(true);
   });
 });
